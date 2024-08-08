705935e98bcd435f633d5ad39eb23db01e1a29cc
Yonela Carbajal <yonela.carbajal@tecsup.edu.pe>
2024-08-07 23:54:24 -0500

    Yonela Validator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+(function() {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function() {
+      var form = document.getElementById("paymentForm");
+      form.addEventListener(
+        "submit",
+        function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          var isValid = true;
+          var inputs = form.querySelectorAll("input, select, textarea");
+          inputs.forEach(function(input) {
+            if (!input.checkValidity()) {
+              isValid = false;
+              input.classList.add("invalid-input");
+            } else {
+              input.classList.remove("invalid-input");
+            }
+          });
+          if (isValid) {
+            document.getElementById("alert").classList.add("d-none");
+          } else {
+            document.getElementById("alert").classList.remove("d-none");
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    },
+    false
+  );
+})();
